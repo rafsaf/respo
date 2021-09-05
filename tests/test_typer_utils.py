5495e228cdc3d3a9dcc375abbc7d9c925eac8a83
rafsaf <rafal.safin12@gmail.com>
2021-09-05 22:32:14 +0200

    finished typer_utils tests

---
+++ tests/test_typer_utils.py
@@ -0,0 +1,19 @@
+from respo.typer_utils import good, bad, FileFormat
+
+
+def test_good():
+    good_text = "AAA BBB"
+
+    assert good(good_text) == "\x1b[32m\x1b[1mINFO: AAA BBB\x1b[0m"
+
+
+def test_bad():
+    bad_text = "BBB AAA"
+    assert bad(bad_text) == "\x1b[33m\x1b[1mERROR: BBB AAA\x1b[0m"
+
+
+def test_file_format():
+    file_format = FileFormat("yml")
+    assert file_format.value == "yml"
+    assert file_format.yml == "yml"
+    assert file_format.json == "json"
