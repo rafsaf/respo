8a8786b877f04710ad466263d2e3b3ca225b194d
rafsaf <rafal.safin12@gmail.com>
2021-09-07 00:28:51 +0200

    added test for typer modify and created_at

--- tests/test_typer.py
+++ tests/test_typer.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from respo import app
+from respo import app, get_respo_model
 from typer.testing import CliRunner
 
 
@@ -38,6 +38,17 @@ def test_respo_create_fail_when_json_sytax_invalid(runner: CliRunner):
 
 def test_respo_create_success_valid_yml_file(runner: CliRunner):
     result = runner.invoke(app, ["create", "tests/cases/general.yml"])
+    assert result.exit_code == 0
+    assert "Success!" in result.stdout
+
+
+def test_respo_create_success_valid_yml_file_2x_modify_created_ok(runner: CliRunner):
+    result = runner.invoke(app, ["create", "tests/cases/general.yml"])
+    model1 = get_respo_model()
+    result = runner.invoke(app, ["create", "tests/cases/general.yml"])
+    model2 = get_respo_model()
+    assert model1.metadata.created_at == model2.metadata.created_at
+    assert not model1.metadata.last_modified == model2.metadata.last_modified
     assert result.exit_code == 0
     assert "Success!" in result.stdout
 
