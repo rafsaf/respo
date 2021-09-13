9f61a9355768890e207776a166ab49c1335bd44b
rafsaf <rafal.safin12@gmail.com>
2021-09-14 01:09:10 +0200

    added result output to index002_cli.sh file in docs

--- docs/examples/index/index002_cli.sh
+++ docs/examples/index/index002_cli.sh
@@ -1,3 +1,11 @@
 respo --help  # prints all available commands
 
 respo create respo-example.yml  # create model from file
+
+# Result
+INFO: Start looking for file 'respo-example.yml'
+INFO: Validating the content...
+INFO: YML syntax validated in 0.004s...
+INFO: Respo model validated in 0.002s...
+INFO: Saving as binary file to .respo_cache/__auto__respo_model.bin
+INFO: Success!
