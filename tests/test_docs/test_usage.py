90974e290c3bea0c1b674b80f943b9cca613bd8b
rafsaf <rafal.safin12@gmail.com>
2022-04-03 20:03:55 +0200

    Added usage files to tests

---
+++ tests/test_docs/test_usage.py
@@ -0,0 +1,4 @@
+def test_docs_index(get_general_model):
+    from docs.examples.usage import main  # noqa
+    from docs.examples.usage import main_roles  # noqa
+    from docs.examples.usage import respo_model  # noqa
