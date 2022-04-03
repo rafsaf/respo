524c96b181ba5fe347bf76a5e332560bac20325f
rafsaf <rafal.safin12@gmail.com>
2022-04-03 20:06:47 +0200

    Change django test secret_key to ranom to avoid warnings

--- tests/test_django_field/respo_field_test/settings.py
+++ tests/test_django_field/respo_field_test/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from typing import List
+import random, string
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-eqt4%ji*c-n#ulgcab^wigodd423_svajsrossg72snv*8@lj%"
+SECRET_KEY = "".join(random.choice(string.ascii_letters) for _ in range(50))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
